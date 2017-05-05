be8154e4c59c7c8083c7e8f4924a064964aec3cb
FlavioSilva <flaviosilva6495@gmail.com>
2017-05-05 11:24:41 -0300

    Listando itens na tela

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,16 @@
 <h1>Testando</h1>
+
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Nome</th>
+    <th>Valor</th>
+  </tr>
+  @foreach($produtos as $produto)
+    <tr>
+      <td>{{$produtos->id}}</td>
+      <td>{{$produtos->nome}}</td>
+      <td>{{$produtos->valor}}</td>
+    </tr>
+  @endforeach
+  </table>
